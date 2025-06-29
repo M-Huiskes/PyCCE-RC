a086361eb0ef6c793687a4a2c72db9def24ed004
Merijn <merijn-huiskes@outlook.com>
2025-06-29 14:02:59 +0200

    removed top layer __init__ definition

--- py_cce/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import data
-from .models import cce_individual, cce_mg, cce_pooled
-from .utils import csa, sample
-
-__all__ = ["data", "sample", "cce_individual", "cce_mg", "cce_pooled", "csa"]
